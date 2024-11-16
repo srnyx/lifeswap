1c66759565eb49a4dbb838e251e245d52b5cc35e
srnyx <25808801+srnyx@users.noreply.github.com>
2024-11-15 20:40:04 -0500

    Update Gradle plugins
    Gradle Galaxy: 1.1.3 -> 1.3.2
    Shadow: 8.1.7 -> 8.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import xyz.srnyx.gradlegalaxy.utility.spigotAPI
 
 plugins {
     java
-    id("xyz.srnyx.gradle-galaxy") version "1.1.3"
-    id("io.github.goooler.shadow") version "8.1.7"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.2"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 setupAnnoyingAPI("723fb94dbb", "xyz.srnyx", "2.0.0", "A combination of LifeSteal and Death Swap")
